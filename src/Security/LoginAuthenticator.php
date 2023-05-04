895eefad476757d0f960fd12b11591221652ef0e
jorCam <jcamacxo@gmail.com>
2023-05-04 10:52:35 +0200

    test api

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -76,8 +76,8 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
-        //return new RedirectResponse($this->urlGenerator->generate('app_formulaire'));
+        //return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_formulaire'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
